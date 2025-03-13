87a92a7cf1fb7ddbb4e3c4121f1a44dfd387e7be
Pranay <pranay.pratap15@gmail.com>
2025-03-14 03:29:23 +0530

    modified:   frontend/package-lock.json 	modified:   frontend/package.json 	new file:   frontend/src/components/LocationSearchPanel.jsx 	modified:   frontend/src/pages/Home.jsx

---
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const LocationSearchPanel = () => {
+  return (
+    <div>
+        <div className='flex items-center my-4 px-3 gap-2'>
+  <div className='bg-gray-200 h-8 w-12 flex items-center justify-center rounded-full'>
+    <i className="ri-map-pin-fill text-black"></i>
+  </div>
+
+    <h4 className='text-base font-medium text-gray-600'>Whitefield Rd, Devasandra Industrial Estate, Mahadevapura, Bengaluru, Karnataka</h4>
+  
+</div>
+    </div>
+  )
+}
+
+export default LocationSearchPanel
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,77 @@
-import React from 'react'
-
+import React, { useRef } from 'react'
+import {useGSAP} from '@gsap/react'
+import gsap from 'gsap'
+import 'remixicon/fonts/remixicon.css'
+import LocationSearchPanel from '../components/LocationSearchPanel'
 const Home = () => {
+  const [pickup, setPickup] = React.useState('');
+  const [destination, setDestination] = React.useState('');
+  const [panelOpen, setPanelOpen] = React.useState(false);
+  const panelRef=useRef(null);
+  const panelCloseRef=useRef(null);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (!pickup || !destination) {
+      alert('Please enter both pickup location and destination');
+      return;
+    }
+    console.log('Trip request submitted:', { pickup, destination });
+    // Here you would typically send this data to a backend API
+    // or dispatch a Redux action to handle the trip request
+    // Reset form after submission (optional)
+    setPickup('');
+    setDestination('');
+  };
+  useGSAP(function(){
+    if(panelOpen){
+      gsap.to(panelRef.current,{
+        height:'70%',
+        opacity:1
+      })
+      gsap.to(panelCloseRef.current,{
+        opacity:1
+      })
+    }else{
+      gsap.to(panelRef.current,{
+        height:'0%',
+        opacity:0
+      })
+      gsap.to(panelCloseRef.current,{
+        opacity:0
+      })
+    }
+  
+   },[panelOpen])
   return (
-    <div>Home</div>
+    <div>
+      <img className='w-16 absolute left-5 top-5' src='https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png' alt="Uber" />
+      <div className='h-screen w-screen'>
+        <img className='h-full w-full object-cover' src="https://media.istockphoto.com/id/1287419541/vector/city-map-vector-illustration.jpg?s=612x612&w=0&k=20&c=0VAHCECbPYP5Yiid8TTcIKKZgzS2rFnowlmIAtcFnOE=" alt="" />
+      </div>
+      <div className='h-screen flex flex-col justify-end absolute top-0 w-full'>
+        <div className='h-[30%] p-6 bg-white relative'>
+          <h5 ref={panelCloseRef} onClick={()=>{
+            setPanelOpen(!panelOpen)
+          }} className='text-2xl opacity-0 absolute top-6 right-6'>
+          <i className="ri-arrow-down-wide-line"></i>
+          </h5>
+        <h4 className='text-2xl font-semibold'>Find a trip</h4>
+        <form onSubmit={(e)=>
+          submitHandler(e)}>
+            <div className='line absolute h-16 w-1 top-[45%] left-10 bg-gray-900 rounded-full'></div>
+          <input value={pickup} onClick={() => setPanelOpen(true)} onChange={(e)=>{
+            setPickup(e.target.value)
+          }} className='bg-[#eee] px-8 py-2 text-lg rounded-base w-full mt-5' type="text" placeholder='Add a pick up location'></input>
+          <input value={destination} onClick={() => setPanelOpen(true)} onChange={(e)=>{
+            setDestination(e.target.value)
+          }} className='bg-[#eee] px-8 py-2 text-lg rounded-base w-full mt-3' type="text" placeholder='Enter your destination'></input>
+        </form>
+        </div>
+        <div ref={panelRef} className='h-0 bg-white'>
+          <LocationSearchPanel />
+        </div>
+      </div>
+    </div>
   )
 }
 
